87cdecee39d129f1ecca7b501d59991604c3a840
Fsmv <kallmeyeras@gmail.com>
2013-02-20 22:39:09 -0500

    Fixed up the tests All tests preform properly now All tests delete the files they generate afterwards now

--- src/test/java/net/sapium/livestreamprocessor/ConcatenatorTest.java
+++ src/test/java/net/sapium/livestreamprocessor/ConcatenatorTest.java
@@ -3,20 +3,21 @@ package net.sapium.livestreamprocessor;
 import java.io.File;
 
 import net.sapium.livestreamprocessor.utils.Concatenator;
+import net.sapium.livestreamprocessor.utils.VideoData;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class ConcatenatorTest extends TestCase {
-    public ConcatenatorTest(String testName){
+    public ConcatenatorTest(String testName) {
         super(testName);
     }
-    
+
     public static Test suite() {
         return new TestSuite(ConcatenatorTest.class);
     }
-    
+
     public void testConcatenate() {
         File file1 = new File("concat1.flv");
         file1.delete();
@@ -24,17 +25,38 @@ public class ConcatenatorTest extends TestCase {
         file2.delete();
         File file3 = new File("concat3.flv");
         file3.delete();
-        
-        File[] fileList = {file1, file2, file3};
-        
+
+        File[] fileList = { file1, file2, file3 };
+
         File outFile = new File("concat.flv");
         outFile.delete();
-        
+
         TestcaseGenerator.generateSplitVideoWithSound(file1.getAbsolutePath(), file2.getAbsolutePath(), file3.getAbsolutePath());
         Concatenator concat = new Concatenator(null, fileList, outFile.getAbsolutePath());
         concat.run();
-        
-        assertTrue(true); //TODO: Some sort of checking to see if the files concatenated correctly
-        //Right now you have to check manually, the gaps in audio are expected (~25ms between videos) becuase my generator leaves empty space at the beginning and end for some reason.
+
+        VideoData outData = new VideoData(outFile);
+        VideoData data1 = new VideoData(file1);
+        VideoData data2 = new VideoData(file2);
+        VideoData data3 = new VideoData(file3);
+
+        long left = outData.getDuration() / 100;
+        long right = (data1.getDuration() + data2.getDuration() + data3.getDuration()) / 100 + 50;
+        boolean aboutEqual = left <= right;
+
+        outData.getReader().close();
+        data1.getReader().close();
+        data2.getReader().close();
+        data3.getReader().close();
+
+        if (aboutEqual) {
+            outFile.delete();
+            file1.delete();
+            file2.delete();
+            file3.delete();
+            assertTrue(true);
+        }else{
+            assertEquals(left, right);
+        }
     }
 }
