04503334c355a7ba56b3218fe52f78e6d9ac2043
Fsmv <kallmeyeras@gmail.com>
2013-02-27 17:35:31 -0500

    Updated pom.xml to make building exes possible

--- buildall.bat
+++ buildall.bat
@@ -1,5 +1,5 @@
-call mvn package -P win32,!win64,!linux32,!linux64,!macosx32,!macosx64
-call mvn package -P !win32,win64,!linux32,!linux64,!macosx32,!macosx64
+call mvn package -P win32,!win64,!linux32,!linux64,!macosx32,!macosx64,exe
+call mvn package -P !win32,win64,!linux32,!linux64,!macosx32,!macosx64,exe
 call mvn package -P !win32,!win64,linux32,!linux64,!macosx32,!macosx64
 call mvn package -P !win32,!win64,!linux32,linux64,!macosx32,!macosx64
 call mvn package -P !win32,!win64,!linux32,!linux64,macosx32,!macosx64
--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mvn package -P win32,!win64,!linux32,!linux64,!macosx32,!macosx64
-mvn package -P !win32,win64,!linux32,!linux64,!macosx32,!macosx64
+mvn package -P win32,!win64,!linux32,!linux64,!macosx32,!macosx64,exe
+mvn package -P !win32,win64,!linux32,!linux64,!macosx32,!macosx64,exe
 mvn package -P !win32,!win64,linux32,!linux64,!macosx32,!macosx64
 mvn package -P !win32,!win64,!linux32,linux64,!macosx32,!macosx64
 mvn package -P !win32,!win64,!linux32,!linux64,macosx32,!macosx64
