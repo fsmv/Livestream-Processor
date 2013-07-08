1625849a80b73b4d1276f9d45cb10337a4d2234d
Andrew Kallmeyer <kallmeyeras@gmail.com>
2013-07-07 21:47:42 -0400

    Update version numbers for the Github release

--- buildall.bat
+++ buildall.bat
@@ -1,6 +1,11 @@
 call mvn package -P win32,!win64,!linux32,!linux64,!macosx32,!macosx64,exe
+
 call mvn package -P !win32,win64,!linux32,!linux64,!macosx32,!macosx64,exe
+
 call mvn package -P !win32,!win64,linux32,!linux64,!macosx32,!macosx64
+
 call mvn package -P !win32,!win64,!linux32,linux64,!macosx32,!macosx64
+
 call mvn package -P !win32,!win64,!linux32,!linux64,macosx32,!macosx64
+
 call mvn package -P !win32,!win64,!linux32,!linux64,!macosx32,macosx64
